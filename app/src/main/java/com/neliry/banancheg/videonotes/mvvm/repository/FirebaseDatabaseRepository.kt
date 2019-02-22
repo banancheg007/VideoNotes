195945b64c84b18572c0f40083750ab20acff8c1
banancheg007 <ruslansolodovnyk@gmail.com>
2019-02-22 17:43:18 +0200

    refactoring

--- app/src/main/java/com/neliry/banancheg/videonotes/mvvm/repository/FirebaseDatabaseRepository.kt
+++ app/src/main/java/com/neliry/banancheg/videonotes/mvvm/repository/FirebaseDatabaseRepository.kt
@@ -7,7 +7,7 @@ import com.google.firebase.database.FirebaseDatabase
 abstract class FirebaseDatabaseRepository<Model>() {
 
     var databaseReference: DatabaseReference
-    protected lateinit var firebaseCallback: FirebaseDatabaseRepositoryCallback<Model>
+
 
 
     protected abstract fun getRootNode(): String
@@ -15,17 +15,5 @@ abstract class FirebaseDatabaseRepository<Model>() {
     init {
         databaseReference =
             FirebaseDatabase.getInstance().getReference("users").child("1OlV0BFqhzNzSMVI0vmoZlTHwAJ2").child(getRootNode())
-    }
-
-    fun addListener(firebaseCallback: FirebaseDatabaseRepositoryCallback<Model>) {
-        this.firebaseCallback = firebaseCallback
-
-    }
-
-
-    interface FirebaseDatabaseRepositoryCallback<T> {
-        fun onSuccess(result: List<T>)
-
-        fun onError(e: Exception)
     }
 }
